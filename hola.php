1eba6ce74597470b4229ae634f16e1a9108f2291
Javi <javier@onesolutions.es>
2020-02-27 14:18:40 +0100

    libreria carbon

--- hola.php
+++ hola.php
@@ -1,3 +1,13 @@
 <?php
+require 'vendor/autoload.php';
+
+use Carbon\Carbon;
+
 echo "hola Mundo";
 echo "nueva linea";
+
+$fecha = Carbon::now();
+
+$fecha->addDays(20);
+
+echo $fecha->format('d-m-Y');
